89900e47a4b2eacea684d07a6a12fa9023512476
DiegoAll <dposadallano@hotmail.com>
2025-10-01 23:47:45 -0500

    namespaces

---
+++ kubernetes-master/k8s-hands-on/backend/src/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"os"
+	"time"
+)
+
+type HandsOn struct {
+	Time     time.Time `json:"time"`
+	Hostname string    `json:"hostname"`
+}
+
+func ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	resp := HandsOn{
+		Time:     time.Now(),
+		Hostname: os.Getenv("HOSTNAME"),
+	}
+
+	jsonResp, err := json.Marshal(&resp)
+	if err != nil {
+		w.Write([]byte("Error"))
+		return
+	}
+	// w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// resp := fmt.Sprintf("La hora es %v y hostname es %v", time.Now(), os.Getenv("HOSTNAME"))
+	// w.Write([]byte(resp))
+	w.Write([]byte(jsonResp))
+
+	// w.Write([]byte(`{"message": "hello world"}`))
+}
+
+func main() {
+
+	http.HandleFunc("/", ServeHTTP)
+	http.ListenAndServe(":9090", nil)
+
+}
